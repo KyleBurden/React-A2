71e242de4693614157abbf468bf0205212a6d045
Kyle Burden <kyleburden709@gmail.com>
2023-02-18 14:10:36 -0330

    useState & useEffect fetch json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+  fetch('./reviewedmovies.json')
+    .then((response) => response.json())
+    .then(setMovies)
+  }, []);
+  console.log(movies);
+
+
   return (
     <div className="App">
       <header className="App-header">
