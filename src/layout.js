a9b6a8dfc07838cc63373f3548e622ee77c99fdc
Kyle Burden <kyleburden709@gmail.com>
2023-02-19 10:32:16 -0330

    header / footer

---
+++ src/layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function Header() {
+    return (
+    <>
+      <header>
+        <h1>KB Movie Review Website:</h1>
+      </header>
+    </>
+    );
+}
+
+function Footer() {
+    return (
+        <>
+        <footer>
+            <p>Assignment completed by #20165611</p>
+        </footer>
+        </>
+    );
+}
