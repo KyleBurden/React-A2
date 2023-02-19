f6a723374ad92745b78323c74d7a828b1452dd27
Kyle Burden <kyleburden709@gmail.com>
2023-02-19 16:53:34 -0330

    code for leave review page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./layout";
+import { Home, LeaveReview } from "./layout";
 
 function App() {
 
@@ -20,6 +20,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home movies={movies} setMovies={setMovies} />} />
+        <Route path="leavereview" element={<LeaveReview movies={movies} setMovies={setMovies} />} />
         </Routes>
     </div>
   );
--- src/layout.js
+++ src/layout.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import {Link} from "react-router-dom";
+
+import { useRef } from 'react';
+
 export function Home({movies, setMovies}) {
     function Movie( {movieName, movieDate, movieActors, moviePoster, movieRating, onRemove = f => f} ) {
         return (
@@ -36,6 +40,68 @@ export function Home({movies, setMovies}) {
                 setMovies(newMovies);
             }} />
             <Footer/>
+        </>
+    );
+}
+export function LeaveReview({movies, setMovies}) {
+    function LeaveReviewForm({onNewMovie = f => f}) {
+        const movieNameText = useRef();
+        const movieDateText = useRef();
+        const movieActorsText = useRef();
+        const movieRatingText = useRef();
+    
+        const submit = e => {
+            e.preventDefault();
+            const movieName = movieNameText.current.value;
+            const movieDate = movieDateText.current.value;
+            const movieActors = movieActorsText.current.value;
+            const movieRating = movieRatingText.current.value;
+            const moviePoster = "/images/default.jpg"
+    
+            onNewMovie(movieName, movieDate, movieActors.split(", "), moviePoster, movieRating);
+            movieNameText.current.value = "";
+            movieDateText.current.value = "";
+            movieActorsText.current.value = [];
+            movieRatingText.current.value = 0;
+        }
+
+        return (
+            <>
+                <form onSubmit={submit}>
+                    <div>
+                        <label>Movie Title:<input ref={movieNameText} type="text" required /></label>
+                    </div>
+                    <div>
+                        <label>Release movieDate:<input ref={movieDateText} type="text" required /></label>
+                    </div>
+                    <div>
+                        <label>Lead Cast:<input ref={movieActorsText} type="text" required /></label>
+                    </div>
+                    <div>
+                    </div>
+                    <div>
+                    <label>Movie Rating:<input ref={movieRatingText} type="text" required/></label>
+                    </div>
+                    <div>
+                        <input type="submit" value="Submit"></input>
+                    </div>
+                </form>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <h1>Add A Review</h1>
+            <nav>
+            <Link to="/">Home</Link>
+            </nav>
+            <br></br>
+            <LeaveReviewForm onNewMovie={(movieName, movieDate, movieActors, moviePoster, movieRating) => {
+                const newMovies= [...movies, {movieName, movieDate, movieActors, moviePoster, movieRating}];
+                setMovies(newMovies)
+            }}
+            />
         </>
     );
 }
