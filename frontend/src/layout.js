f778920cd524d05a795ddd09b89d67175e3769d0
Kyle Burden <kyleburden709@gmail.com>
2023-03-02 20:07:12 -0330

    Fixed frontend

--- frontend/src/layout.js
+++ frontend/src/layout.js
@@ -1,51 +1,91 @@
 //test
 import React from "react";
 
+import { Navigate } from "react-router-dom";
+
+import { useState} from 'react';
+
 import {Link} from "react-router-dom";
 
 import { useRef } from 'react';
 import {Navbar, Nav } from 'react-bootstrap';
 
 export function Home({movies, setMovies}) {
-    function Movie( {movieName, movieDate, movieActors, moviePoster, movieRating, onRemove = f => f} ) {
+
+    function Movie( {name, date, actors, poster, rating, onRemove = f => f} ) {
+
+        const actorsList = actors.map( (actor, i) => {
+            if (i + 1 === actors.length){
+                return <span key={i}>{actor}</span>
+            } else {
+                return <span key={i}>{actor}, </span>
+            }
+        });
+
         return (
           <>
-            <h2>{movieName}</h2>
-            <img 
-                src={process.env.PUBLIC_URL + moviePoster} 
-                alt={movieName + " Movie Poster"}>
-            </img>
-            <h3>Release Date: {movieDate}</h3>
-            <h3>Actors: {(movieActors).join(", ")}</h3>
-            <h3>Rating: {movieRating} Stars</h3>
-            <button onClick={() => onRemove(movieName)}>Remove</button>
+                    <h3 className="">  {name}</h3>
+                    <img 
+                    className="border" 
+                    rounded 
+                    src={poster} alt={name + " Movie Poster"}>
+                    </img>
+
+                    <div>
+                        <p>Release Date</p>
+                        <p>{date}</p>
+                        <p> Lead Actors</p>
+                        <p>{actorsList}</p>
+                        <p> Rating</p>
+                        <p>{rating} out of 5</p>
+                    </div>
+                
+                <button onClick={ evt => onRemove(name)}></button>
+                
           </>
         );
-    }
+    };
     function MovieList( { movies = [], onRemoveMovie = f => f}) {
+        if (!movies.length) return <div>No movies available.</div>;
         return (
             <>
                 {movies.map( movie => (
-                    <Movie key={movie.movieName} {...movie} onRemove={onRemoveMovie} />
+                    <Movie key={movie.name} {...movie} onRemove={onRemoveMovie} />
                 ))}
             </>
         );
-    }
+    };
 
     return (
         <>
             <Header />
-            <MovieList 
-                movies={movies} 
-                onRemoveMovie={ movieName => {
-                const newMovies = movies.filter(movie => movie.movieName !== movieName);
-                setMovies(newMovies);
-            }} />
-            <Footer/>
+            <div>
+            <MovieList
+                        movies={movies} 
+                        onRemoveMovie={ 
+                            name => {
+                            const removeMovie = async () => {
+                                const movieRemoved = await fetch("/api/removeMovie", 
+                                    {method: "post", 
+                                    body: JSON.stringify({name: name}),
+                                    headers: {"Content-Type": "application/json"}
+                                    });
+                                const body = await movieRemoved.json();
+                                if (body.message !== "Unable to delete movie") {
+                                    const newMovies = movies.filter(movie => movie.name !== name);
+                                    setMovies(newMovies);
+                                }
+                            }
+                            removeMovie();
+                        }
+                    } />
+                </div>
+            <footer/>
         </>
     );
 }
 export function LeaveReview({movies, setMovies}) {
+
     function LeaveReviewForm({onNewMovie = f => f}) {
         const movieNameText = useRef();
         const movieDateText = useRef();
@@ -55,39 +95,43 @@ export function LeaveReview({movies, setMovies}) {
     
         const submit = e => {
             e.preventDefault();
-            const movieName = movieNameText.current.value;
-            const movieDate = movieDateText.current.value;
-            const movieActors = movieActorsText.current.value;
-            const movieRating = movieRatingText.current.value;
-            const moviePoster = "/images/default.jpg"
+
+            const formData = new FormData();
+
+            formData.append("name", movieNameText);
+            formData.append("date", movieDateText);
+            formData.append("actors", [movieActorsText.split(",")])
+            formData.append("poster", poster);
+            formData.append("rating", movieRatingText);
+
+            onNewMovie(formData);
+            
     
-            onNewMovie(movieName, movieDate, movieActors.split(", "), moviePoster, movieRating);
             movieNameText.current.value = "";
             movieDateText.current.value = "";
             movieActorsText.current.value = [];
-            movieRatingText.current.value = 0;
+            setPoster("");
+            movieRatingText.current.value = "";
+
+            Navigate('/', { replace: true});
         }
 
         return (
             <>
-                <form onSubmit={submit}>
-                    <div>
-                        <label>Movie Title:<input ref={movieNameText} type="text" required /></label>
-                    </div>
-                    <div>
-                        <label>ReleaseDate:<input ref={movieDateText} type="text" required /></label>
-                    </div>
-                    <div>
-                        <label>Lead Cast:<input ref={movieActorsText} type="text" required /></label>
-                    </div>
-                    <div>
-                    </div>
-                    <div>
-                    <label>Movie Rating:<input ref={movieRatingText} type="text" required/></label>
-                    </div>
-                    <div>
-                        <input type="submit" value="Submit"></input>
-                    </div>
+                <form onSubmit={submit} method="post" action="/upload_files" encType="multipart/form-data">
+                            <div>
+                                <label className="form-label h4">Movie Poster:<input className="form-control" type="file" accept=".png,.jfif,.jpg,.jpeg"
+                                onChange = {e => setPoster(e.target.files[0])} required /></label>
+                            </div>
+                            <div>
+                                <label className="form-label h4">Movie Title:<input className="form-control" ref={movieNameText} type="text" required /></label>
+                            </div>
+                            <div>
+                                <label className="form-label h4">Release Date:<input className="form-control" ref={movieDateText} type="text" required /></label>
+                            </div>
+                            <div>
+                                <label className="form-label h4">Lead Cast:<input className="form-control" ref={movieActorsText} type="text" required /></label>
+                            </div>
                 </form>
             </>
         );
@@ -100,11 +144,23 @@ export function LeaveReview({movies, setMovies}) {
             <Link to="/">Home</Link>
             </nav>
             <br></br>
-            <LeaveReviewForm onNewMovie={(movieName, movieDate, movieActors, moviePoster, movieRating) => {
-                const newMovies= [...movies, {movieName, movieDate, movieActors, moviePoster, movieRating}];
-                setMovies(newMovies)
+            <LeaveReviewForm onNewMovie={async (formData) => {
+                const addMovie = async () => {
+                const movieAdded = await fetch("/api/addMovie", {
+                    method: "post",
+                    body: formData
+                    
+                });
+                const body = await movieAdded.json();
+                    if (body.message === "Success") {
+                        setMovies(body.movies)
+                    }
+                }
+                    
+                addMovie();
             }}
             />
+            <Footer/>
         </>
     );
 }
@@ -113,20 +169,15 @@ function Header() {
     return (
     <>
       <header>
-        <h1>KB Movie Reviews</h1>
-        <div>
-        <Navbar bg="dark" variant="dark"> 
-            <Nav>
-            <Nav.Link href="/"> Home</Nav.Link>
-            <Nav.Link href="/leavereview"> Leave a review </Nav.Link>
-            </Nav>
-        
-        </Navbar>
-        </div>
+        <h1>KB Movie Review Website:</h1>
+        <nav>
+            <Link to="LeaveReview">Click here to leave a new review</Link>
+        </nav>
       </header>
     </>
     );
 }
+
 
 function Footer() {
     return (
